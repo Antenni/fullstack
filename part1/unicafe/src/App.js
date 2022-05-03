4a617148389bf6ff987805829741c28fc8b67753
Antenni <antenni79@gmail.com>
2022-05-03 13:44:00 +0300

    1.10 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,9 +9,13 @@ const Button = (props) => {
   )
 }
 
-const Value = ({text,value}) => {
+const StatisticLine = (props) => {
   return (
-    <p>{text} {value}</p>
+    <div>
+      <p>
+        {props.text} {props.value}
+        </p>
+    </div>
   )
 }
 
@@ -24,12 +28,12 @@ const Statistics = (props) => {
 }
   return (
     <div>
-      <Value text="good" value={props.good}/>
-      <Value text="neutral" value={props.neutral}/>
-      <Value text="bad" value={props.bad}/>
-      <Value text="all" value={props.all}/>
-      <Value text="average" value={props.average}/>
-      <Value text="positive" value={props.positive.toFixed(1) +' %'}/>
+      <StatisticLine text="good" value={props.good}/>
+      <StatisticLine text="neutral" value={props.neutral}/>
+      <StatisticLine text="bad" value={props.bad}/>
+      <StatisticLine text="all" value={props.all}/>
+      <StatisticLine text="average" value={props.average}/>
+      <StatisticLine text="positive" value={props.positive.toFixed(1) +' %'}/>
     </div>
   )
 }
