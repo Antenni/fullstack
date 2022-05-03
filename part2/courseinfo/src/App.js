e53793a1d3bbd234d4cf7ed45c665c020f747493
Antenni <antenni79@gmail.com>
2022-05-03 15:56:04 +0300

    2.5 done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,39 +1,4 @@
-import React from 'react'
-
-const Header = ({name}) => {
-  return (
-    <h1>{name}</h1>
-  )
-}
-const Part = (props) => {
-  return (
-    <p>{props.name} {props.exercises}</p>
-  )
-}
-
-const Total = ({parts}) => {
-  return (
-    <h4>total of {parts.reduce((s, p) => s + p.exercises, 0)} exercises</h4>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <div>
-    {parts.map(part =>
-      <Part key={part.id} name={part.name} exercises={part.exercises}/>)}
-    </div>
-  )
-}
-const Course = ({course}) => {
-  return (
-    <div>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from './Course'
 
 const App=() => {
   const courses = [
@@ -87,4 +52,4 @@ const App=() => {
     </div>
   );
 }
-export default App;
+export default App
