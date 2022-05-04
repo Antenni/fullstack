6d3ee7205cad5cf5500be197db1f64c3aafd49ac
Antti Nygård SOFTS21P <Antti.Nygard2@centria.fi>
2022-05-04 10:34:33 +0300

    2.9 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,20 @@ import { useState } from 'react'
 const App = () => {
   
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',number: '040-44-1234567' }]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 } ])
   
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [newSearch, setNewSearch] = useState('')
+
+    const result = persons.filter(person => person.name.includes(newSearch))
+
+    const addNewSearch = (event) => {
+      setNewSearch(event.target.value)
+    }
 
   const addNewName = (event) => {
     setNewName(event.target.value)
@@ -35,6 +45,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input value={newSearch} onChange={addNewSearch} />
+      <h2>add a new</h2>
       <form onSubmit={addAccount}>
         <div>
           name: <input value={newName} onChange={addNewName} />
@@ -47,7 +59,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person,p) =>
+      {result.map((person,p) =>
       <div key={p}>{person.name} {person.number}</div>)}
     </div>
   )
