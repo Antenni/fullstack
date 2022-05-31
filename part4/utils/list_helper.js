84167aee0b37cb0da86d7a9b254a80ccbb0c71bd
Antenni <antenni79@gmail.com>
2022-05-31 22:18:14 +0300

    4.4 done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
     return 1
   }
+  const totalLikes = (blogs) => {
+    return (
+      blogs.map(blog => blog.likes)
+      .reduce((total, likes) => total + likes, 0)
+    )
+  }
 
   module.exports = {
-    dummy
+    dummy, totalLikes
   } 
