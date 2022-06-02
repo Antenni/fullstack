ca9aee2a416bc8573a785521d1ef1f948f19c552
Antenni <antenni79@gmail.com>
2022-06-02 14:20:00 +0300

    4.15 done

--- part4/app.js
+++ part4/app.js
@@ -1,12 +1,16 @@
 const config = require('./utils/config')
 const express = require('express');
-require('express-async-errors');
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+//require('express-async-errors');
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+
+
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -20,6 +24,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
